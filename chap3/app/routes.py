708d3e1f5cca47602f5e03bfa08c6bde4ba11735
taran19971 <59025984+taran19971@users.noreply.github.com>
2019-12-19 17:25:06 +0530

    Create routes.py

---
+++ chap3/app/routes.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Taran'}
+    posts = [
+        {
+            'author': {'username': 'Karan'},
+            'body': 'Beautiful day in Pune!'
+        },
+        {
+            'author': {'username': 'Sanya'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
